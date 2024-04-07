1cbe02c266eb7a75cdcb8e510f1e78d6ebc981e9
hmabuhurera <160245362+hmabuhurera@users.noreply.github.com>
2024-04-08 02:56:43 +0500

    todo_ist _short_project

---
+++ main.js
@@ -0,0 +1,24 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+let toDos = [];
+let condition = true;
+while (condition) {
+    let todoQuestion = await inquirer.prompt([
+        {
+            name: "todoAsking",
+            type: "input",
+            message: chalk.yellow("What whoult you like to add in your todo?"),
+        },
+        {
+            name: "todoGuaid",
+            type: "confirm",
+            message: chalk.blue("Would you like to add other thing in your todos"),
+            default: chalk.green("true"),
+        }
+    ]);
+    toDos.push(todoQuestion.todoAsking);
+    console.log(chalk.redBright(toDos));
+    condition = todoQuestion.todoGuaid;
+}
+;
